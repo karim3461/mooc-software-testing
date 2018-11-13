f2c935474a588b46691cfeb7e692e4948252ae48
karim <karim3461@gmail.com>
2018-11-13 14:45:59 -0500

    Restored original RomanNumeralTest class

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,49 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    @Test
+    public void mirrorWith2() {
+        String result = new Mirror().mirrorEnds("abXYZba");
+        Assertions.assertEquals("ab", result);
+    }
+
+    @Test
+    public void mirrorWith1() {
+        String result = new Mirror().mirrorEnds("abca");
+        Assertions.assertEquals("a", result);
+    }
+
+    @Test
+    public void mirrorWith3() {
+        String result = new Mirror().mirrorEnds("aba");
+        Assertions.assertEquals("aba", result);
+    }
+
+    @Test
+    public void mirrorOK() {
+        String result = new Mirror().mirrorEnds("abccba");
+        Assertions.assertEquals("abc", result);
+    }
+
+    @Test
+    public void mirrorNotOK() {
+        String result = new Mirror().mirrorEnds("abcba");
+        Assertions.assertEquals("ab",result);
+    }
+
+    @Test
+    public void mirrorOneChar() {
+        String result = new Mirror().mirrorEnds("a");
+        Assertions.assertEquals("a",result);
+    }
+
+    @Test
+    public void mirrorNone() {
+        String result = new Mirror().mirrorEnds("ab");
+        Assertions.assertEquals("",result);
+    }
 }
--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -8,36 +8,30 @@ public class RomanNumeralTest {
 
     private RomanNumeral roman;
 
-    @BeforeEach
-    public void initialize() {
-        //System.out.print("This method is called before each test!\n");
-        this.roman = new RomanNumeral();
-    }
-
     @Test
     public void singleNumber() {
-        //RomanNumeral roman = new RomanNumeral();
+        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void numberWithManyDigits() {
-        //RomanNumeral roman = new RomanNumeral();
+        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        //RomanNumeral roman = new RomanNumeral();
+        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        //RomanNumeral roman = new RomanNumeral();
+        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
