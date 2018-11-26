e075cf8f0bf02d40c1ab0c25e167df7986b01b82
karim <karim3461@gmail.com>
2018-11-26 17:46:57 -0500

    Added tests for CountLetters class

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -7,14 +7,32 @@ public class CountLettersTest {
 
     @Test
     public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
+        int words = new CountLetters().count("cats|tiger");
         Assertions.assertEquals(2, words);
     }
 
     @Test
     public void lastWordDoesNotMatch() {
-        int words = new CountLetters().count("cats|dog");
+        int words = new CountLetters().count("tiger|dog");
         Assertions.assertEquals(1, words);
     }
+
+    @Test
+    public void firstWordDoesNotMatch() {
+        int words = new CountLetters().count("cat|dogs");
+        Assertions.assertEquals(1, words);
+    }
+
+    /**@Test
+    public void noMatchingWord() {
+        int words = new CountLetters().count("cat|dog");
+        Assertions.assertEquals(0, words);
+    }*/
+
+    /**@Test
+    public void emptyString() {
+        int words = new CountLetters().count("");
+        Assertions.assertEquals(0, words);
+    }*/
 
 }
